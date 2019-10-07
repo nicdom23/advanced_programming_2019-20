787ab2c0e4a4769a4a9dfd773666cd92796eb402
nicdom23 <nicoladomenis23@hotmail.it>
2019-10-07 11:32:17 +0200

    Added the first set of exercises

---
+++ exercises/c++/01_intro/first_lesson/test.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+//#include <cmath>
+
+int main(){
+
+int h{};
+std::cout<<"the variable h{} has been initialized as a " << h << std::endl;
+
+double d1{0.22};
+double d2{0.22};
+std::cout<<"numbers " << d1 << " and "<< d2<<std::endl;
+
+c = abs(d1-d2) < 1e-16;
+//(void) c;
+std::cout<<"boolean is"<< c<<std::endl;
+return 0;
+}
